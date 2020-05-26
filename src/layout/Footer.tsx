4001bde2363330513eff90145f507b896ae2e110
victorh3 <dvictorhu@gmail.com>
2020-05-26 10:40:49 -0500

    Match styles

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,7 +2,14 @@ import React, { FunctionComponent } from "react";
 import "./Layout.scss";
 
 const Footer: FunctionComponent = () => {
-  return <footer className="Footer">Footer</footer>;
+  return (
+    <footer className="Footer">
+      <h2 className="Footer__Title">
+        <span className="Footer__Title--mobile">F</span>
+        <span className="Footer__Title--desktop">Footer</span>
+      </h2>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,7 +2,14 @@ import React, { FunctionComponent } from "react";
 import "./Layout.scss";
 
 const Header: FunctionComponent = () => {
-  return <header className="Header">Header</header>;
+  return (
+    <header className="Header">
+      <h2 className="Header__Title">
+        <span className="Header__Title--mobile">H</span>
+        <span className="Header__Title--desktop">Header</span>
+      </h2>
+    </header>
+  );
 };
 
 export default Header;
